20157f5d9c58094fe9fa5c3003da213ac4cc3255
Daniel Rocha <daniel1990rocha@gmail.com>
2021-04-05 09:54:33 -0300

    Inserindo novos campos na tabela Recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
 	def index
-		@recipes = [ 'Strogonoff de carne', 'Filé de frango a milanesa', 'Salmão ao forno', 'Bacalhau ao forno']
+		@recipes = Recipe.all
 	end
 end
